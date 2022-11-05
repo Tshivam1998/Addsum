79cf0619a8484e229a1ee24377330aa48845631b
Shivam <tshivam@telaverge.com>
2022-11-05 13:27:17 +0530

    finding the sum of two numbers here

---
+++ src/Other.java
@@ -0,0 +1,9 @@
+public class Other {
+    public static void main(String[] args) {
+        int a =10;
+        int b=90;
+        int c=a+b;
+        System.out.println(c);
+        System.out.println("yh h phle wala isme kya kr skte h");
+    }
+}
